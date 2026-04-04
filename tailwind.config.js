40015c9572e517dc342b3a073036542b3f744d04
aman1357 <amancorp04@gmail.com>
2026-04-04 23:54:56 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
